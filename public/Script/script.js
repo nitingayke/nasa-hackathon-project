380e56b796f94ed20f516ee94f7efa78f5573b65
Nitin Gayke <gaykenitin975@gmail.com>
2024-10-06 03:21:10 +0530

    added new logic

--- public/Script/script.js
+++ public/Script/script.js
@@ -1,1 +1,20 @@
-const box = document.querySelector(".intro");
+const lessonQuery = document.querySelectorAll(".lession-query");
+const lessionBox = document.querySelectorAll(".lesson-answers");
+
+for(let box of lessonQuery){
+    box.addEventListener("click", (event) => {
+        let lessionBoxId = box.id;
+
+        for(let lbox of lessionBox){
+            if(lbox.classList.contains("visible")){
+                if(lessionBoxId == lbox.id)
+                    return ;
+
+                lbox.classList.remove("visible");
+            }
+            if(lbox.id == lessionBoxId){
+                lbox.classList.add("visible");
+            }
+        }
+    });
+}
